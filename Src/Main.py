f29e32e2c6ac422f69c6258048946a2ba831b02b
gomerthegoose <gomerthegoose@gmail.com>
2023-05-04 15:51:20 +0100

    edge points

--- Src/Main.py
+++ Src/Main.py
@@ -22,7 +22,7 @@ font = pygame.font.SysFont(None, 30)
 # set up colors
 white = (255, 255, 255)
 BackGroundColour = (27, 30, 43)
-pointColour = (0, 0, 0)
+pointColour = (255,255,255)
 lineColour = (255,255,255)
 delta_time = 1
 
@@ -40,7 +40,7 @@ def addPointMarker(point,text=" <--",):
     # draw FPS text to top left corner of screen
     screen.blit(markerText, point)
     
-def createPoints(num_points = 3):
+def createPoints(num_points = 3,numExternalPoints = 10):
     # set up random points
     points = []
     for i in range(num_points):
@@ -48,30 +48,50 @@ def createPoints(num_points = 3):
         y = random.randint(0, screen_height)
         point = [x, y]
         points.append(point)
+    
+    for x in range(numExternalPoints):
+        point = [(x+ 1) * round(screen_width/numExternalPoints),0]
+        points.append(point)
+        point = [x * round(screen_width/numExternalPoints),screen_height]
+        points.append(point)
+        
+    for y in range(numExternalPoints):
+        point = [0,y * round(screen_height/numExternalPoints)]
+        points.append(point)
+        point = [screen_width,(y + 1) * round(screen_height/numExternalPoints)]
+        points.append(point)
     return points
 
-def AddInitialVelocities(num_points):
+def AddInitialVelocities(num_points,numExternalPoints = 10):
     velocities = []
+    print (range(num_points))
     for i in range(num_points):
-        dx = random.uniform(-1, 1)
-        dy = random.uniform(-1, 1)
-        velocity = [dx, dy]
-        velocities.append(velocity)
+        if(i < num_points - numExternalPoints *4):
+            dx = random.uniform(-1, 1)
+            dy = random.uniform(-1, 1)
+            velocity = [dx, dy]
+            velocities.append(velocity)
+        else:
+            velocities.append([0,0])
     return velocities
 
-def CreatePointSizes(num_points, avarageSize= 8, maxSize = 1):
+def CreatePointSizes(num_points, avarageSize= 3, maxSize = 1,numExternalPoints = 10):
     PointSizes = []
-    for i in range(num_points):
-        PointSizes.append(random.normalvariate(avarageSize,maxSize))
+    for i in range(num_points):    
+        if(i < num_points - numExternalPoints *4):
+            PointSizes.append(random.normalvariate(avarageSize,maxSize))
+        else:
+            PointSizes.append(0)
+
     return PointSizes
 
-def DrawPoints(points, num_points, pointColour,pointSizes,zoom =1):
+def DrawPoints(points, num_points, pointColour,pointSizes):
     for i in range(num_points):
         pygame.gfxdraw.filled_circle(screen, int(points[i][0]), int(points[i][1]), int(pointSizes[i]),pointColour)
         pygame.gfxdraw.aacircle(screen, int(points[i][0]), int(points[i][1]), int(pointSizes[i]),pointColour)
         #pygame.draw.circle(screen, pointColour, (int(points[i][0]), int(points[i][1])), int(pointSizes[i]))
         
-def update_points(points, velocities, dt, screen_width, screen_height, point_radius):
+def update_points(points , velocities, dt, screen_width, screen_height, point_radius):
     for i in range(len(points)):
         # Update position based on velocity
         points[i][0] += velocities[i][0] * dt
@@ -111,7 +131,7 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 print("Space bar pressed down!")
-                PointVelocities[0] = [100,10]
+                PointVelocities[0] = [10,10]
                 
                 
     # fill screen with white
@@ -131,16 +151,18 @@ while running:
     
         
     for simplex in tri.simplices:
-        poly = [PointLocations[simplex[0]],PointLocations[simplex[1]],PointLocations[simplex[1]],PointLocations[simplex[2]],PointLocations[simplex[2]],PointLocations[simplex[0]]]
+        #poly = [PointLocations[simplex[0]],PointLocations[simplex[1]],PointLocations[simplex[1]],PointLocations[simplex[2]],PointLocations[simplex[2]],PointLocations[simplex[0]]]
+        poly = [PointLocations[simplex[0]],PointLocations[simplex[1]],PointLocations[simplex[2]]]
         avarageY = (PointLocations[simplex[0]][1] + PointLocations[simplex[1]][1] + PointLocations[simplex[2]][1]) / 3
         
-        lineColour = (255-int(255*avarageY/screen_height),int(255*avarageY/screen_height),0)
+        lineColour = (int(255*avarageY/screen_height),20,255-int(255*avarageY/screen_height))
         pygame.gfxdraw.filled_polygon(screen,poly,lineColour)
+        #pygame.gfxdraw.aapolygon(screen,poly,(255,255,255))
 
     # draw points as small circles
     DrawPoints(PointLocations,num_points,pointColour,pointSizes)
     
-    addPointMarker(PointLocations[0])
+    #addPointMarker(PointLocations[0])
     addFpsCounter()
     # update screen
     pygame.display.update()
